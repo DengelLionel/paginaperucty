49d79beef351840743d0eaf586b626c7440143bd
DengelLionel <dengelriveracajincho@gmail.com>
2024-10-03 12:08:35 -0500

    ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,10 +16,10 @@ export default function Home() {
 
     <div>
       <Head>
-      <title>Crea tienda ya | Creación y mejora de tiendas online shopify</title>
+      <title>Diseñamos y rediseñamos tu tienda online shopify Perú</title>
       <meta property="og:image" content="https://res.cloudinary.com/dh9etf988/image/upload/v1705081869/creatiendaya/LOGO_CTY_VERDE_02_iauov1.png" />
-<meta name="description" content="Creamos tu tienda online. Somos expertos en diseños de tiendas online y estamos comprometidos en ayudarte a lanzar y hacer prosperar tu negocio en línea de manera efectiva y exitosa. ¡Descubre cómo podemos hacer que tu visión cobre vida en el mundo digital hoy mismo!" />
-<meta name="keywords" content="crear tienda online, tienda virtual, tiendas online Perú, tiendas virtuales gratis, tienda en línea, ecommerce, creación de tiendas, negocio en línea, dropshipping, emprendimiento,emprendedor,tienda virtual ecuador,tienda virtual colombia, tienda virtual chile, tienda virtual mexico" />
+<meta name="description" content="Creamos tu tienda online. Diseñamos tu tiendas online y estamos comprometidos en ayudarte a lanzar y hacer prosperar tu negocio  de manera efectiva y exitosa. Con diseño personalizado en tu tienda online" />
+<meta name="keywords" content="ayuda en tienda online, tienda virtual para Perú, tiendas online Perú, tienda virtual precio economico, experto en tienda en línea, necesito ayuda en mi tienda online, negocio en línea, dropshipping en Perú, emprendimiento,emprendedor" />
 <meta name="robots" content="index, follow" />
         <link rel="icon" href="/crea_tienda_ya_favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
@@ -40,7 +40,7 @@ export default function Home() {
   {/* <HechaVistasoProyectos/> */}
     {/*  <PlantillasWeb/> */}
      <Preguntas/>
-     <Action link='https://wa.link/zqg997' titulo='Potencia tu tienda online existente' description='Mejoramos tu tienda online con un rediseño espectacular. Escribenos para más información.' boton='QUIERO MEJORAR MI TIENDA' imagen='https://res.cloudinary.com/dh9etf988/image/upload/v1709345698/creatiendaya/tienda_online_peru_hhfxer.png'/>
+     <Action link='https://wa.link/zqg997' titulo='Rediseña tu tienda online' description='Mejoramos tu tienda online con un rediseño espectacular. Escribenos para más información.' boton='QUIERO MEJORAR MI TIENDA' imagen='https://res.cloudinary.com/dh9etf988/image/upload/v1709345698/creatiendaya/tienda_online_peru_hhfxer.png'/>
    {/*   <Formulario/> */}
      <Whatsapp/>
       </main>
